2ce07ffa9211faa0047460d176fa5974c44117fc
AGarciaNY <aldairgarcia803@gmail.com>
2019-08-12 00:20:39 -0400

    lost in time

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React,{Component} from 'react'
+//import { View, Text } from 'react-native';
 import './App.css';
-// import firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import Nav from './components/nav';
 import Footer from './components/footer';
 import Home from './components/home';
 import ArtWork from './components/art-work';
-import  Projects from './components/projects';
+import Projects from './components/projects';
+import AppComponent from './appcomponent';
+// import firebaseConfig from './config';
 
-export default class App extends Component{
+export default class App extends AppComponent{
+  
+  constructor(props) {
+    super(props);
+    this.getSessionDatabaseRef().update({
+      hostScore: 0,
+      guestScore: 0,
+      hasGameStarted: false,
+      timelefts: 0,
+      timeleftm: 0
+    });
+    this.state = {
+      pickapage: 'first',
+    }
+  }
+
   // componentDidMount(){
-  //   // Your web app's Firebase configuration
   //   var firebaseConfig = {
   //     apiKey: "AIzaSyBybFMb2UKqxDtBzxsbDYerGlc5xotyC90",
   //     authDomain: "react-portfolio-4cfd9.firebaseapp.com",
@@ -21,35 +38,24 @@ export default class App extends Component{
   //   };
   //   // Initialize Firebase
   //   firebase.initializeApp(firebaseConfig);
-
-  //   firebase.database().ref('users/001').set(
+  //   firebase.database().ref('users/002').set(
   //     {
-  //       name:'this testing',
-  //       age: 21,
-  //     }
-  //   ).then(()=>{
-  //     console.log('Inserted !');
-  //   }).catch((error) => {
-  //     console.log(error)
-  //   });
-  //   firebase.database().ref();
+  //       name:'me',
+  //       age:22,
+  //     }then(()=)
+  //   )
+  //   console.log(firebase.database);
   // }
-  constructor() {
-    super();
-    this.state = {
-      pickapage: 'default',
-    }
-  }
 
   pickAPage = (page) => {
-  this.setState({
-    pickapage: page
-  });
-  console.log(page);
+    this.setState({
+      pickapage: page
+    });
+    console.log(page);
   }  
 
   render(){
-    if(this.state.pickapage === 'default'){
+    if(this.state.pickapage === 'first'){
       return (
       <div id="body">
         <Nav changepage={(page)=> this.pickAPage(page)}/>
@@ -62,30 +68,30 @@ export default class App extends Component{
     if(this.state.pickapage === 'home'){
       return (
         <div id="body">
-        <Nav changepage={(page)=> this.pickAPage(page)}/>
-        <Home />
-        <Footer />
-      </div>
+          <Nav changepage={(page)=> this.pickAPage(page)}/>
+          <Home />
+          <Footer />
+        </div>
       )
     }
 
     if(this.state.pickapage === 'artwork'){
       return (
         <div id="body">
-        <Nav changepage={(page)=> this.pickAPage(page)}/>
-        <ArtWork />
-        <Footer />
-      </div>
+          <Nav changepage={(page)=> this.pickAPage(page)}/>
+          <ArtWork />
+          <Footer />
+        </div>
       )
     }
 
     if(this.state.pickapage === 'projects'){
       return (
         <div id="body">
-        <Nav changepage={(page)=> this.pickAPage(page)}/>
-        <Projects />
-        <Footer />
-      </div>
+          <Nav changepage={(page)=> this.pickAPage(page)}/>
+          <Projects />
+          <Footer />
+        </div>
       )
     }
   }
